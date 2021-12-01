812c7f108bd634f4a5401a34f137ad3cab562bd3
“minhanh” <vuongminhanhsnail@gmail.com>
2021-12-01 11:21:10 +0700

    init

--- helloworld_ios/ViewController.swift
+++ helloworld_ios/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var firstNumberLb: UILabel!
+    @IBOutlet var secondNumberLb: UILabel!
+    @IBOutlet var operatorLb: UILabel!
+    @IBOutlet var resultLb: UILabel!
+    @IBOutlet var nextBtnOutlet: UIButton!
+    @IBAction func nextBtn(_ sender: Any) {
+        randomOp()
+    }
+    func randomOp() {
+        let arithmeticOperators = ["+", "-", "*", "/"]
+        let fistNumber = Int.random(in: 1...1000)
+        let secondNumber = Int.random(in: 1...1000)
+        firstNumberLb.text = String(fistNumber)
+        secondNumberLb.text = String(secondNumber)
+        let choice = arithmeticOperators.randomElement()!
+        operatorLb.text = choice
+        switch choice {
+        case "+":
+            resultLb.text = String(fistNumber + secondNumber)
+        case "-":
+            resultLb.text = String(fistNumber - secondNumber)
+        case "*":
+            resultLb.text = String(fistNumber*secondNumber)
+        case "/":
+            resultLb.text = String(fistNumber/secondNumber)
+        default:
+            print("No")
+        }
+    }
+    func customNextBtnUI() {
+        self.nextBtnOutlet.layer.cornerRadius = 20.0
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomOp()
+        customNextBtnUI()
         // Do any additional setup after loading the view.
-    }
-
-
+    }    
 }
 
